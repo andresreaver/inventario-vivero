b24c3c0c74afe9e42708a85fb2ae7e209a82063c
carloshernandez176264 <carloshernandez176264@hotmail.com>
2024-12-10 23:49:38 -0500

    crud de productos

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from gestion.views.user_views import admin_dashboard, logout_view
-from .views import user_views, producto_views, compra_views, compras
+from .views import user_views, producto_views, compra_views, compras, crear_producto, editar_producto, eliminar_producto
 from .views.informe_views import informes
 from .views.venta_views import ventas
 
@@ -10,11 +10,15 @@ urlpatterns = [
     path('logout/', user_views.logout_view, name='logout'),
     path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
     path('productos/', producto_views.lista_productos, name='lista_productos'),
+    path('productos/crear/', crear_producto, name='crear_producto'),
+    path('productos/editar/<int:producto_id>/', editar_producto, name='editar_producto'),
+    path('productos/eliminar/<int:producto_id>/', eliminar_producto, name='eliminar_producto'),
     path('compras/', compra_views.lista_compras, name='lista_compras'),
     path('password-reset/', user_views.password_reset_request, name='password_reset_request'),
     path('reset-password/<uidb64>/<token>/', user_views.password_reset, name='password_reset'),
     path('compras/', compras, name='compras'),
     path('ventas/', ventas, name='ventas'),
     path('informes/', informes, name='informes'),
+
 
 ]
--- gestion/views/producto_views.py
+++ gestion/views/producto_views.py
@@ -1,8 +1,61 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from gestion.services.producto_service import ProductoService
+from django.contrib import messages
 
 @login_required
 def lista_productos(request):
     productos = ProductoService.obtener_productos()
     return render(request, 'lista_productos.html', {'productos': productos})
+
+@login_required
+def crear_producto(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        tipo = request.POST['tipo']
+        stock = int(request.POST['stock'])
+        stock_minimo = int(request.POST['stock_minimo'])
+        fecha_caducidad = request.POST.get('fecha_caducidad', None)
+
+        try:
+            ProductoService.crear_producto(nombre, tipo, stock, stock_minimo, fecha_caducidad)
+            messages.success(request, f"Producto {nombre} creado exitosamente.")
+            return redirect('lista_productos')
+        except Exception as e:
+            messages.error(request, f"Error al crear producto: {str(e)}")
+
+    return render(request, 'crear_producto.html')
+
+@login_required
+def editar_producto(request, producto_id):
+    producto = ProductoService.obtener_producto_por_id(producto_id)
+    if request.method == 'POST':
+        datos = {
+            'nombre': request.POST['nombre'],
+            'tipo': request.POST['tipo'],
+            'stock': int(request.POST['stock']),
+            'stock_minimo': int(request.POST['stock_minimo']),
+            'fecha_caducidad': request.POST.get('fecha_caducidad', None)
+        }
+
+        try:
+            ProductoService.actualizar_producto(producto_id, **datos)
+            messages.success(request, f"Producto {datos['nombre']} actualizado correctamente.")
+            return redirect('lista_productos')
+        except Exception as e:
+            messages.error(request, f"Error al actualizar producto: {str(e)}")
+
+    return render(request, 'editar_producto.html', {'producto': producto})
+
+@login_required
+def eliminar_producto(request, producto_id):
+    producto = get_object_or_404(ProductoService.obtener_productos(), id=producto_id)
+    if request.method == 'POST':
+        try:
+            ProductoService.eliminar_producto(producto_id)
+            messages.success(request, f"Producto {producto.nombre} eliminado correctamente.")
+            return redirect('lista_productos')
+        except Exception as e:
+            messages.error(request, f"Error al eliminar producto: {str(e)}")
+
+    return render(request, 'eliminar_producto.html', {'producto': producto})
